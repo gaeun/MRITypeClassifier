0f6447e15486a6c1b8e0f3a66b66eea367952ed3
Gaeun Kim <gaeunkim913@gmail.com>
2016-11-26 20:48:38 +0900

    Complete Gabor Filter Features

--- gaborFilterFeatures/gaborFilterFeatures.m
+++ gaborFilterFeatures/gaborFilterFeatures.m
@@ -2,6 +2,8 @@ default = '/Users/allisonkim/Desktop/TCGA-GBM';
 paths = {'0003','0034','0037','0047','0048','0054','0068','0086','0106','0116','0164','0165','0166','0175','0213','0214','0649','0790','1395','1396','1401','1453','1458','1794','1795','3650'};
 featuresflair = [];
 featurespost = [];
+gaborArray = gaborFilterBank(5,8,39,39);
+
 for i = 1:25
    path = [default '/' paths{i}];
    pathdir = dir(path);
@@ -27,13 +29,11 @@ for i = 1:25
    
    Iflair = volumeflair(:,:,floor((4+length(path3dir))/2)); % Extract the middle slice
    normIflair = Iflair / max(Iflair(:)); % Normalize the middle slice (0-1)
-   glcmflair = graycomatrix(normIflair); % Generate GLCM
-   haralickFlair(i,:) = haralickTextureFeatures(glcmflair); % Extract Haralick features for glcm
+   featureVector = gaborFeatures(normIflair,gaborArray,4,4);
    
    Ipost = volumepost(:,:,floor((4+length(path3dir))/2)); % Extract the middle slice
    normIpost = Ipost / max(Ipost(:)); % Normalize the middle slice (0-1)
-   glcmpost = graycomatrix(normIpost); % Generate GLCM
-   haralickPost(i,:) = haralickTextureFeatures(glcmpost); % Extract Haralick features for glcm
+   featureVector = gaborFeatures(normIpost,gaborArray,4,4);
 
 end
 
